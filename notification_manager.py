5215af6556c33d5b1cc192d594d53bb799f196f4
M1LAN <pc@MacBook-Air.local>
2024-01-28 19:15:07 +0100

    hide sensitive info

--- notification_manager.py
+++ notification_manager.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account_sid = "ACaaa5fed9a247130bc352309b9997c947"
-auth_token = "2dd0b2ac79d29168c65b8a233691ce2b"
+account_sid = "your SID"
+auth_token = "your TOKEN"
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
@@ -14,8 +14,8 @@ class NotificationManager:
         message = client.messages \
                         .create(
                              body=message,
-                             from_='+15188643603',
-                             to='+421944436207'
+                             from_='+number',
+                             to='+number'
                          )
 
         print(message.status)
